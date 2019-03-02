17ba3450d57399ba03d4e0fd55580e3a7d8de0a3
Dan Shields <d.shieldsuk@gmail.com>
2019-03-02 03:57:35 +0000

    feat: add twitch embed and banner logo

--- src/views/Announcement.vue
+++ src/views/Announcement.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="announcement">
-
+    <img class="banner" src="../assets/img/banner.png"/>
     <Countdown date="March 30, 2019 10:00"/>
+    <div id="twitch-embed"></div>
   </div>
 </template>
 
@@ -12,6 +13,27 @@ export default {
   name: 'announcement',
   components: {
     Countdown
+  },
+  mounted () {
+    /* eslint-disable */
+    new Twitch.Embed("twitch-embed", {
+      width: 760,
+      height: 428,
+      layout: "video",
+      channel: "uomesports"
+    });
+    /* eslint-enable */
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.announcement {
+  width: 1000px;
+  margin: auto;
+}
+.banner {
+  width: 90%;
+  margin-top: 50px;
+}
+</style>
